0275b0f1e7ec1d5776960b5251ed6fb91e5bb316
Lucas Nery <79020288+lucasonery@users.noreply.github.com>
2022-01-17 21:45:32 -0300

    Update #6.c

--- #6.c
+++ #6.c
@@ -4,40 +4,40 @@
 
 void preenche(int matriz[9][9]){
 
-int i, j;
-srand(time(0));    
-for ( i=0; i<9; i++ ){
-    for ( j=0; j<9; j++ ) {
-         matriz[i][j] = rand()%10;
-     }
-}
-return;
+    int i, j;
+    srand(time(0));    
+    for ( i=0; i<9; i++ ){
+        for ( j=0; j<9; j++ ) {
+             matriz[i][j] = rand()%10;
+         }
+    }
+    return;
 }
 
 void imprime(int matriz[9][9]){
 
-int i, j;
-for ( i=0; i<9; i++ ){
-    for ( j=0; j<9; j++ ) {
-         printf("%d", matriz[ i ][ j ]);
+    int i, j;
+    for ( i=0; i<9; i++ ){
+        for ( j=0; j<9; j++ ) {
+             printf("%d", matriz[ i ][ j ]);
+        }
     }
-}
-return;
+    return;
 }
 
 int main(){
 
-int **matriz;
-int i, j;
-    
-matriz = malloc(9*sizeof(int*));
+    int **matriz;
+    int i, j;
 
-for(i = 0; i < 9; i++){
-    matriz[i] = malloc(9*sizeof(int));
-}
+    matriz = malloc(9*sizeof(int*));
 
-preenche(matriz);
-imprime(matriz);
+    for(i = 0; i < 9; i++){
+        matriz[i] = malloc(9*sizeof(int));
+    }
+
+    preenche(matriz);
+    imprime(matriz);
 
 return 0;
 }

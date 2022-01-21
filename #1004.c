c65d92669f07428316092b3b924d50510e835450
Lucas Nery <79020288+lucasonery@users.noreply.github.com>
2022-01-21 16:37:45 -0300

    Create #1004.c

---
+++ #1004.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main(){
+    
+    int multi, a, b; // variáveis
+    
+    scanf("%d %d", &a, &b);  //lê as entradas
+    multi = a * b;   //multiplica as variáveis
+    
+    printf("PROD = %d\n", multi); //imprime a resposta
+    
+    return 0;
+}

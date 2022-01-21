a88b0992d73cd63c2da0f01f7919f0b2dac35f24
Lucas Nery <79020288+lucasonery@users.noreply.github.com>
2022-01-21 16:35:09 -0300

    Create #1003.c

---
+++ #1003.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main(){
+    
+    int soma, a, b;
+    
+    scanf("%d %d", &a, &b);  //lê as entradas
+    soma = a + b;   //soma as variáveis
+    
+    printf("SOMA = %d\n", soma); //imprime a resposta
+    
+    return 0;
+}

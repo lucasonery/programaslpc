2116552e6fa8d503b8fe644d55549beb78c9a446
Lucas Nery <79020288+lucasonery@users.noreply.github.com>
2022-01-21 18:44:37 -0300

    Create #1051.c

---
+++ #1051.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    float renda, imposto;
+    scanf("%f", &renda);
+
+    if ((renda > 0.00) && (renda <= 2000.00)){
+        printf("Isento\n");
+    }
+    else{
+        if ((renda > 2000.00) && (renda <= 3000.00)){
+            imposto = (renda - 2000.00)*0.08;    
+            printf("R$ %.2f\n", imposto);
+        }
+        else{
+            if ((renda > 3000.00) && (renda <= 4500.00)){
+                imposto = ((1000)*0.08 + (renda - 3000.00)*0.18); 
+                printf("R$ %.2f\n", imposto);
+            }
+            else{
+                if (renda > 4500.00){
+                    imposto = ((1000)*0.08 + (1500)*0.18 + (renda - 4500)*0.28); 
+                    printf("R$ %.2f\n", imposto);
+                }
+            }
+        }
+    }
+    
+return 0;
+}

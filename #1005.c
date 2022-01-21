a3727189d9738b2ae8648670d136a7898a3f57fe
Lucas Nery <79020288+lucasonery@users.noreply.github.com>
2022-01-21 16:49:57 -0300

    Update #1005.c

--- #1005.c
+++ #1005.c
@@ -4,10 +4,10 @@ int main(){
     
     float media, nota1, nota2; // variáveis
     
-    scanf("%f %f", &nota1, &nota2);  //lê as entradas
+    scanf("%f %f %f", &nota1, &nota2);  //lê as entradas
     media = (nota1*3.5 + nota2*7.5)/(3.5+7.5); // media das notas
     
-    printf("MEDIA = %5f\n", media); //imprime a resposta
+    printf("MEDIA = %.5f\n", media); //imprime a resposta
     
     return 0;
 }
